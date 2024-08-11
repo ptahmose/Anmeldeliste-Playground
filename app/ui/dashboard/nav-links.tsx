ce5e4715755181f6e96fea1f2d57bd7a74b33f52
ptahmose <jbohl@h-quer.de>
2024-08-11 18:03:16 +0200

    update

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -23,6 +23,7 @@ const links = [
 
 export default function NavLinks() {
   const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
@@ -36,8 +37,8 @@ export default function NavLinks() {
               {
                 'bg-sky-100 text-blue-600': pathname === link.href,
               },
-            )}          
-            >
+            )}
+          >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
           </Link>
